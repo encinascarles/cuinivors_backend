a5807cf595a65e1faa055b1d6dedefadc550ab2c
Carles Encinas <carles@ganxo.net>
2024-03-22 12:00:07 +0100

    DB connection and bcrypt use

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,38 +1,62 @@
-import asyncHandler from 'express-async-handler';
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 // @desc    Auth user & get token
 // @route   POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  res.send('auth user');
+  res.send("auth user");
 });
 
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.send('register user');
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 // @desc    Logout user / clear cookie
 // @route   POST /api/users/logout
 // @access  Public
 const logoutUser = (req, res) => {
-  res.send('logout user');
+  res.send("logout user");
 };
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('get profile');
+  res.send("get profile");
 });
 
 // @desc    Update user profile
 // @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send('update profile');
+  res.send("update profile");
 });
 
 export {
