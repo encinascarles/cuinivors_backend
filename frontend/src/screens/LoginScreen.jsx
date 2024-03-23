48c95ce1b4415c92983a10b9c876b32aadee6604
Carles Encinas <carles@ganxo.net>
2024-03-23 12:45:45 +0100

    Implemented toastify messages

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useLoginMutation } from "../slices/userApiSlice";
 import { setCredentials } from "../slices/authSlice";
+import { toast } from 'react-toastify';
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
@@ -32,7 +33,7 @@ const LoginScreen = () => {
       console.log("3");
       navigate("/");
     } catch (err) {
-      console.log(err?.data?.message || err.error);
+      toast.error(err?.data?.message || err.error);
     }
   };
 
