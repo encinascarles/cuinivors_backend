de142a04879829c46694202783e93a9d9442195c
Carles Encinas <carles@ganxo.net>
2024-03-23 13:16:56 +0100

    logout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,27 @@
 import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useLogoutMutation } from '../slices/userApiSlice';
+import { logout } from '../slices/authSlice';
+import { toast } from 'react-toastify';
 
 const Header = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const dispatch = useDispatch();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      toast.success('Logged out successfully');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -9,6 +30,10 @@ const Header = () => {
         </Typography>
         <Button color="inherit">Sign In</Button>
         <Button color="inherit">Sign Up</Button>
+        {userInfo && (
+          <Button color="inherit" onClick={logoutHandler }>Log Out</Button>
+        )}
+        
       </Toolbar>
     </AppBar>
   );
