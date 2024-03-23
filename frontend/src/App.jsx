75843d73235f6ce59e90d7d4d7c20af52969815c
Carles Encinas <carles@ganxo.net>
2024-03-23 12:38:09 +0100

    Toast container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
-
+import { ToastContainer } from 'react-toastify';
 const App = () => {
   return (
     <>
       <CssBaseline enableColorScheme />
       <Header />
+      <ToastContainer />
       <Outlet />
     </>
   );
