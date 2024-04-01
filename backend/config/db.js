501199d62b705ab7b51fa7bb0612b8457d947099
Carles Encinas <carles@ganxo.net>
2024-04-01 23:37:37 +0200

    Solved Mongo warnings

--- backend/config/db.js
+++ backend/config/db.js
@@ -17,10 +17,7 @@ const connectDB = async () => {
     mongoUri = process.env.MONGO_URI;
   }
   try {
-    const conn = await mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(mongoUri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
